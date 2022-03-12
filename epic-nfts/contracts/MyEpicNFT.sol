f4e209d67d6f2f3e7a18ad544bdbf80b984fde94
yokinist <ikd618@gmail.com>
2022-03-12 14:20:37 +0900

    feat: onchain nft

--- epic-nfts/contracts/MyEpicNFT.sol
+++ epic-nfts/contracts/MyEpicNFT.sol
@@ -16,7 +16,7 @@ contract MyEpicNFT is ERC721URIStorage {
   Counters.Counter private _tokenIds;
 
   // NFT トークンの名前とそのシンボルを渡します。
-  constructor() ERC721 ("TanyaNFT", "TANYA") {
+  constructor() ERC721 ("SquareNFT", "SQUARE") {
     console.log("This is my NFT contract.");
   }
 
@@ -30,7 +30,7 @@ contract MyEpicNFT is ERC721URIStorage {
     _safeMint(msg.sender, newItemId);
 
     // NFT データを設定します。
-    _setTokenURI(newItemId, "https://jsonkeeper.com/b/C931");
+    _setTokenURI(newItemId, "data:application/json;base64,eyJuYW1lIjoiRXBpY05mdENyZWF0b3IiLCJkZXNjcmlwdGlvbiI6IlRoZSBoaWdobHkgYWNjbGFpbWVkIHNxdWFyZSBjb2xsZWN0aW9uIiwiaW1hZ2UiOiJkYXRhOmltYWdlL3N2Zyt4bWw7YmFzZTY0LFBITjJaeUI0Yld4dWN6MGlhSFIwY0RvdkwzZDNkeTUzTXk1dmNtY3ZNakF3TUM5emRtY2lJSEJ5WlhObGNuWmxRWE53WldOMFVtRjBhVzg5SW5oTmFXNVpUV2x1SUcxbFpYUWlJSFpwWlhkQ2IzZzlJakFnTUNBek5UQWdNelV3SWo0S0lDQWdJRHh6ZEhsc1pUNHVZbUZ6WlNCN0lHWnBiR3c2SUhkb2FYUmxPeUJtYjI1MExXWmhiV2xzZVRvZ2MyVnlhV1k3SUdadmJuUXRjMmw2WlRvZ01UUndlRHNnZlR3dmMzUjViR1UrQ2lBZ0lDQThjbVZqZENCM2FXUjBhRDBpTVRBd0pTSWdhR1ZwWjJoMFBTSXhNREFsSWlCbWFXeHNQU0ppYkdGamF5SWdMejRLSUNBZ0lEeDBaWGgwSUhnOUlqVXdKU0lnZVQwaU5UQWxJaUJqYkdGemN6MGlZbUZ6WlNJZ1pHOXRhVzVoYm5RdFltRnpaV3hwYm1VOUltMXBaR1JzWlNJZ2RHVjRkQzFoYm1Ob2IzSTlJbTFwWkdSc1pTSStSWEJwWTA1bWRFTnlaV0YwYjNJOEwzUmxlSFErQ2p3dmMzWm5QZz09In0=");
 
     // NFTがいつ誰に作成されたかを確認します。
     console.log("An NFT w/ ID %s has been minted to %s", newItemId, msg.sender);
