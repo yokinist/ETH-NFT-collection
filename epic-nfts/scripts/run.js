ec0daba540fdeba42d524044f69fafabeafc155e
yokinist <ikd618@gmail.com>
2022-03-12 13:33:53 +0900

    feat: run epic nft on local

--- epic-nfts/scripts/run.js
+++ epic-nfts/scripts/run.js
@@ -1,14 +1,18 @@
 const main = async () => {
-  // コントラクトがコンパイルします
-  // コントラクトを扱うために必要なファイルが `artifacts` ディレクトリの直下に生成されます。
   const nftContractFactory = await hre.ethers.getContractFactory("MyEpicNFT");
-  // Hardhat がローカルの Ethereum ネットワークを作成します。
   const nftContract = await nftContractFactory.deploy();
-  // コントラクトが Mint され、ローカルのブロックチェーンにデプロイされるまで待ちます。
   await nftContract.deployed();
   console.log("Contract deployed to:", nftContract.address);
+  // makeAnEpicNFT 関数を呼び出す。NFT が Mint される。
+  let txn = await nftContract.makeAnEpicNFT();
+  // Minting が仮想マイナーにより、承認されるのを待つ。
+  await txn.wait();
+  // makeAnEpicNFT 関数をもう一度呼び出す。NFT がまた Mint される。
+  txn = await nftContract.makeAnEpicNFT();
+  // Minting が仮想マイナーにより、承認されるのを待つ。
+  await txn.wait();
 };
-// エラー処理を行っています。
+
 const runMain = async () => {
   try {
     await main();
