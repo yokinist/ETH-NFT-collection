144b513aaa1b3163553632800417fbdf310836a0
yokinist <ikd618@gmail.com>
2022-03-13 14:12:13 +0900

    chore: update contract

--- frontend/src/constants/index.js
+++ frontend/src/constants/index.js
@@ -1,6 +1,6 @@
-export const MINT_PRICE = 0.001;
+export const MINT_PRICE = "0.001";
 export const MAX_SUPPLY = 100;
 export const RINKEBY_CHAIN_ID = "0x4";
-export const CONTRACT_ADDRESS = "0x9788f96123952ce4635B932BE0441567b79026Ab";
+export const CONTRACT_ADDRESS = "0x4B9dE361b1a68Ae6b72CD1dcB3909295Af2653C1";
 export const TWITTER_HANDLE = "0xYokinist";
 export const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
