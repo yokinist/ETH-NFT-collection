dc6eb30f8e107dadee402f37c553ffc60d16dd0a
yokinist <ikd618@gmail.com>
2022-03-13 17:08:26 +0900

    chore: compile contract

--- frontend/src/constants/index.js
+++ frontend/src/constants/index.js
@@ -1,6 +1,6 @@
 export const MINT_PRICE = "0.001";
 export const MAX_SUPPLY = 100;
 export const RINKEBY_CHAIN_ID = "0x4";
-export const CONTRACT_ADDRESS = "0x4B9dE361b1a68Ae6b72CD1dcB3909295Af2653C1";
+export const CONTRACT_ADDRESS = "0x79A157A8819cE98A501d03c7076463a6b03940e5";
 export const TWITTER_HANDLE = "0xYokinist";
 export const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
